fe112ccc5704d15289e48febbf46692504c62aa6
jeffreyvanelderen <jeffrey.vanelderen@acagroup.be>
2024-03-19 09:31:48 +0100

    Improve switch

--- WeatherManager.swift
+++ WeatherManager.swift
@@ -19,17 +19,17 @@ struct WeatherManager {
         var icon = "";
         
         switch id {
-        case let id where id < 300:
+        case 200...300:
             icon = "cloud.bolt"; break;
-        case let id where id < 500:
+        case 400...500:
             icon = "cloud.snow"; break;
-        case let id where id < 600:
+        case 500...600:
             icon = "cloud.rain"; break;
-        case let id where id < 700:
+        case 600...700:
             icon = "cloud.snow"; break;
         case let id where id == 800:
             icon = "sun.max"; break;
-        case let id where id < 900:
+        case 801...900:
             icon = "cloud"; break;
         default:
             icon = "";
@@ -60,7 +60,7 @@ struct WeatherManager {
         
         if let data = data {
             // {"coord":{"lon":5.3056,"lat":50.8755},"weather":[{"id":801,"main":"Clouds","description":"few clouds","icon":"02d"}],"base":"stations","main":{"temp":1.85,"feels_like":-2.56,"temp_min":0.92,"temp_max":2.89,"pressure":1008,"humidity":72},"visibility":10000,"wind":{"speed":4.92,"deg":230,"gust":5.81},"clouds":{"all":15},"dt":1705414561,"sys":{"type":2,"id":2003943,"country":"BE","sunrise":1705390479,"sunset":1705420891},"timezone":3600,"id":2803285,"name":"Alken","cod":200}
-
+            
             // let json = String(data: data, encoding: .utf8)!;
             
             return decode(data);
